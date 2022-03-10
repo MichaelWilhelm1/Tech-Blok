b7fb95eacdaa86baeeeb6c54d52d7243d4aa0502
Michael Wilhelm <mwilhelm08@gmail.com>
2022-03-10 09:47:53 +0100

    Week 4 - database toegevoegd

--- app.js
+++ app.js
@@ -4,6 +4,16 @@ const app = express()
 
 const port = 3000
 
+const dotenv = require('dotenv').config();
+const {
+    MongoClient
+} = require('mongodb');
+const {
+    ObjectId
+} = require('mongodb');
+
+let db = null;
+
 app.use(express.static('public'))
 
 app.set('view engine', 'ejs');
@@ -38,10 +48,28 @@ app.get('*', function (req, res) {
     res.send('CANNOT FIND PAGE ERROR 404 (oepsie)', 404);
 
 });
+
+/*****************************************************
+ * Connect to database
+ ****************************************************/
+async function connectDB() {
+    const uri = process.env.DB_URI;
+    const client = new MongoClient(uri, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    });
+    try {
+        await client.connect();
+        db = client.db(process.env.DB_NAME);
+    } catch (error) {
+        throw error;
+    }
+}
+
 /*webserver starten*/
 
 app.listen(port, () => {
 
     console.log(`Example app listening on port ${port}`)
-
+    connectDB().then(() => console.log("We have a connection to Mongo!"));
 })
