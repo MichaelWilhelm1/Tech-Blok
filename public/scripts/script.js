00d235b55d38ed5ba3f92b5933f8b74ea0913fa6
Michael Wilhelm <mwilhelm08@gmail.com>
2022-03-12 21:52:53 +0100

    tressvolle weekend

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -19,9 +19,71 @@ function openmenu() {
 
 }
 
+//fetch("https://restcountries.com/v3.1/all")
+//    .then(function (res) {
+//        //console.log(res);
+//        return res.json();
+//    })
+//    .then(function (data) {
+//       //console.log(data);
+//        initialize(data);
+//    })
+//    .catch(function (err) {
+//        console.log("error", err);
+//    });
+let countries; // hier zit de data in
+
+fetch("https://restcountries.com/v2/all")
+    .then(res => res.json())
+    .then(data => initialize(data))
+    .catch(err => console.log("error", err));
+
+
+function initialize(countriesData) {
+    countries = countriesData;
+
+    let land = "";
+    for (let i = 0; i < countries.length; i++) {
+        land += `<option value="${countries[i].alpha3Code}">${countries[i].name} (+${countries[i].callingCodes[0]})</option>`;
+    }
+    document.getElementById("countries").innerHTML = land;
+
+    let valuta = "";
+    for (let i = 0; i < countries.length; i++) {
+        valuta += `<option value="${countries[i].alpha3Code}">${countries[i].timezones[0]}</option>`;
+    }
+    document.getElementById("timezone").innerHTML = valuta;
+
+    let regio = "";
+    for (let i = 0; i < countries.length; i++) {
+        regio += `<option value="${countries[i].alpha3Code}">${countries[i].region}</option>`;
+    }
+    document.getElementById("regio").innerHTML = regio;
+
+    let taal = "";
+    for (let i = 0; i < countries.length; i++) {
+        taal += `<option value="${countries[i].alpha3Code}">${countries[i].languages[0].name}</option>`;
+    }
+    document.getElementById("language").innerHTML = taal;
+
+}
+
+/*
+    .then((results) => {
+
+        return results.json();
+
+    })
+
+    .then((data) => {
+
+        console.log(data);
+
+    }); */
+
 function getUsers() {
 
-    fetch("https://restcountries.com/v3.1/all")
+    fetch("https://restcountries.com/v2/all")
 
         .then((results) => {
 
