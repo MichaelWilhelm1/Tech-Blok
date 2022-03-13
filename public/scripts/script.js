610280fef1aa8c635c4a4ffb039b880e976c3117
Michael Wilhelm <mwilhelm08@gmail.com>
2022-03-13 17:48:18 +0100

    Nog meer stress

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -19,18 +19,108 @@ function openmenu() {
 
 }
 
-//fetch("https://restcountries.com/v3.1/all")
-//    .then(function (res) {
-//        //console.log(res);
-//        return res.json();
-//    })
-//    .then(function (data) {
-//       //console.log(data);
-//        initialize(data);
-//    })
-//    .catch(function (err) {
-//        console.log("error", err);
-//    });
+
+const countriesEl = document.getElementById('countries');
+const filterknop = document.getElementById('filter');
+const regioknoppen = filterknop.querySelectorAll('li');
+const zoekveld = document.getElementById('search');
+
+getCountries();
+
+async function getCountries() {
+    const res = await fetch('https://restcountries.com/v2/all');
+    const countries = await res.json();
+
+    displayCountries(countries);
+}
+
+function displayCountries(countries) {
+    /* empty string */
+    countriesEl.innerHTML = '';
+    /* countries for each is een loop */
+    countries.forEach(country => {
+        const countryEl = document.createElement('div');
+        countryEl.classList.add('card');
+
+        countryEl.innerHTML =
+            `               
+                 <div class="">
+                    <div>
+                        <img src="${country.flag}" alt="germany">
+                    </div>
+                    <div class="card-body">
+                        <h2 class="country-name">${country.name}</h2>
+                        <p><strong>Populatie:</strong>
+                            ${country.population}
+                        </p>
+                        <p class="country-region"><strong>Regio:</strong>
+                            ${country.region}
+                        </p>
+                        <p><strong>Hoofdstad:</strong>
+                            ${country.capital}
+                        </p>
+                        <p><strong>Taal:</strong>
+                            ${country.languages[0].name}
+                        </p>
+                    </div>
+                </div>`;
+
+        countriesEl.appendChild(countryEl);
+    });
+}
+
+getinfo();
+
+async function getinfo() {
+    const res = await fetch('https://restcountries.com/v2/all');
+    const countries = await res.json();
+
+    console.log(countries);
+}
+
+// openen en sluiten met display block en none in de css
+filterknop.addEventListener('click', () => {
+    filterknop.classList.toggle('open');
+});
+
+//zoek een land
+zoekveld.addEventListener('input', e => {
+    const {
+        value
+    } = e.target;
+    const landnaam = document.querySelectorAll('.country-name');
+
+    landnaam.forEach(name => {
+        if (name.innerText.toLowerCase().includes(value.toLowerCase())) {
+            name.parentElement.parentElement.style.display = 'block';
+        } else {
+            name.parentElement.parentElement.style.display = 'none';
+        }
+    });
+});
+
+// de filter op regio knop
+regioknoppen.forEach(filter => {
+    filter.addEventListener('click', () => {
+        const value = filter.innerText;
+        const landregio = document.querySelectorAll('.country-region');
+
+        landregio.forEach(region => {
+            if (region.innerText.includes(value) || value === 'All') {
+                region.parentElement.parentElement.style.display = 'block';
+            } else {
+                region.parentElement.parentElement.style.display = 'none';
+            }
+        });
+    });
+});
+
+
+
+/*
+
+oude code! (ander probeersel)
+
 let countries; // hier zit de data in
 
 fetch("https://restcountries.com/v2/all")
@@ -81,7 +171,12 @@ function initialize(countriesData) {
 
     }); */
 
-function getUsers() {
+
+
+
+
+/*
+function getCountries() {
 
     fetch("https://restcountries.com/v2/all")
 
@@ -93,10 +188,11 @@ function getUsers() {
 
         .then((data) => {
 
-            console.log(data);
+            displayCountries(countries);
 
         });
 
 }
 
-getUsers();
+getCountries();
+*/
